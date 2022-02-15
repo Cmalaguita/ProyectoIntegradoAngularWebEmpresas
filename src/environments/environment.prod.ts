5d065967fda6fafd6208f571bcfac927d1f49190
Cmalaguita <72543773+Cmalaguita@users.noreply.github.com>
2022-02-15 20:42:10 +0100

    cambio en la url de la api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true
 };
-export const apiURL='http://51.254.98.185/api'
+export const apiURL='https://api.infocarlos.me/api'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL:'http://localhost:5000/api'
+  apiURL:'https://api.infocarlos.me/api'
 };
 export const apiURL='https://api.infocarlos.me/api'
 /*
